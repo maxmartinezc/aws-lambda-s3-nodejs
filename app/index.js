5628a988c28530b252b9ea3895e89be4bad26d7e
Max Martinez <max.martinez.c@gmail.com>
2023-02-14 18:33:49 +1300

    Update response message

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("AWS Lambda and S3 Bucket")
+    body: JSON.stringify("AWS Lambda and S3 Bucket - New version")
   };
   return response;
 }
